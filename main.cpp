99029278a5db2105055ae0a34bbc48031b9c03ab
fabbem <fabbem@pop-os.localdomain>
2023-06-20 23:28:44 +0200

    Some more work done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
-#include <cstring>
-#include <string>
+#include "tester.cpp"
+#include <thread>
+
+using namespace std;
+
 // från start av program båda listorna null
 //
 // if input == char:
@@ -12,8 +14,47 @@
 //        input -> lista1
 //
 
-using namespace std;
-
 int main() {
-return 0;
+    
+    float leftcolumn;
+    int leftcount;
+    float rightcolumn;
+    int rightcount;
+
+    char method;
+    int key;
+    float sum;
+
+    leftcolumn = 0;
+    rightcolumn = 0;
+
+    leftcount = 0;
+    rightcount = 0;
+
+    sum = 0;
+    method = '0';
+    
+    while (true) {
+        key = getch();
+        // Om ingen method är funnen så 
+        // kollar vi vilken lista vi ska
+        // lägga key-pressen i.
+        if (is_Char(key)) {
+            method = key;        
+            cout << method << endl;
+        } else {            
+            if (is_varEmpty(method)) {
+                leftcolumn = calculate(key, 0.0, leftcolumn);
+                leftcount++;
+            } else {
+                rightcolumn = calculate(key, 0.0, rightcolumn);
+                rightcount++;
+            }
+        }
+
+        cout << leftcolumn << " " << rightcolumn << endl;
+    }
+
+    return 0;
+
 }
--- tester.cpp
+++ tester.cpp
@@ -1,27 +1,79 @@
-#include <iostream>
 #include <cstring>
 #include <string>
+#include <iostream>
+#include <termios.h>
+#include <unistd.h>
 
-bool is_listEmpty(char* x) {
-  return std::strlen(x) == 0;
+
+// Check if list is empty
+bool is_varEmpty(char x) {
+    return x == 0;
 }
 
-
+// Function that checks if variable is Char
 bool is_Char(char x) {
     return (typeid(x) == typeid(char));
 }
 
-int main() {
-    char x = 'x';
+char getch() {
+    char ch;
+    struct termios oldSettings, newSettings;
 
-    std::cout << is_Char(x) << std::endl;
+    // Save current terminal settings
+    tcgetattr(STDIN_FILENO, &oldSettings);
+    newSettings = oldSettings;
 
+    // Disable terminal echo
+    newSettings.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newSettings);
+
+    // Read a single character from the user
+    read(STDIN_FILENO, &ch, 1);
+
+    // Restore original terminal settings
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldSettings);
+
+    return ch;
 }
+
+
+float calculate(int x, float y, float result) {
+    // Multiply the result by 10 and add x to the ones place
+    result = result * 10 + x;
+
+    // Convert y to an integer by truncating the decimal part
+    int yAsInt = static_cast<int>(y);
+
+    // Multiply the result by 10 and add yAsInt to the ones place
+    result = result * 10 + yAsInt;
+
+    return result;
+}
+
+
+//   1 - 12
+//   if 
+
+//int main() {
+//    std::cout << "Enter a character: ";
+//    char input = getch();
+//    std::cout << "\nYou entered: " << input << std::endl;
+//
+//    return 0;
+//}
+
+
+//
+//int main() {
+//    char x = 'x';
+//
+//    std::cout << is_Char(x) << std::endl;
+//
+//}
 //char x[1] = {'*'};
 
 //int main() {
 //    std::cout << is_listEmpty(x) << std::endl;
 //  return 0;
 //}
-
 
