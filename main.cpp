dbb49d195840fa6f2cf65e30db39529e95080648
fabbem <fabbem@pop-os.localdomain>
2023-06-20 18:38:43 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+// från start av program båda listorna null
+//
+// if input == char:
+//    input -> lista2
+// 
+// if lista2 not_empty():
+//    input -> lista3
+//    else:
+//        input -> lista1
+//
+
+using namespace std;
+
+bool not_empty(x){
+  if x[0] != null
+}
+
+
+
+
+
+
+
+int main() {
+  return 0;
+}
---
+++ tester.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+
+bool is_listEmpty(char* x) {
+  return strlen(x) == 0;
+}
+
+
+
+
+
+
+
+
+
+char x[1] = '*';
+
+
+
+
+
+
+
+int main() {
+  cout << is_listEmpty(x) << endl;
+  return 0;
+}
